69f06f158e6c5daec0d43c4b486afc1d822b4d86
blakedt2280 <blake_turner@stu.indianhills.edu>
2021-01-27 11:05:39 -0600

    working login and profile page

--- pc_builder/builder/urls.py
+++ pc_builder/builder/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name = 'builder-home'),
-    path('login/', views.login, name = 'builder-login'),
     path('builds/', views.builds, name = 'builder-builds'),
     path('about/', views.about, name = 'builder-about'),
 ]
--- pc_builder/users/views.py
+++ pc_builder/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -14,8 +15,8 @@ def register(request):
 		if (form.is_valid() and request.POST.get('address') != ''):
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}!')
-			
+			messages.success(request, f'Account created. You are now able to login.')
+			return redirect('login')
 
 		else:
 			if request.POST.get('address') == '':
@@ -26,3 +27,8 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form, 'err' : errmsg})
+
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
