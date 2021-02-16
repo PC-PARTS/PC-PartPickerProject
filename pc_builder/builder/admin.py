9dca1e4a8cf93f31c78b88f9e4f3da569bba8086
bruhbruh445 <alexreed2001@gmail.com>
2021-02-16 11:20:18 -0600

    bug fixes but and models

--- pc_builder/builder/admin.py
+++ pc_builder/builder/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Build, Cpu, Cpu_Cooler, Motherboard, Memory1, Memory2, Storage1, Storage2, Storage3, Gpu, Case, Psu, Os, Monitor1, Monitor2, Monitor3
+from .models import Build, Cpu, Cpu_Cooler, Motherboard, Memory1, Memory2, Storage1, Storage2, Storage3, Gpu, Case, Psu, Monitor1, Monitor2, Monitor3
 
 admin.site.register(Build)
 # Register your models here.
@@ -14,7 +14,6 @@ admin.site.register(Storage3)
 admin.site.register(Gpu)
 admin.site.register(Case)
 admin.site.register(Psu)
-admin.site.register(Os)
 admin.site.register(Monitor1)
 admin.site.register(Monitor2)
 admin.site.register(Monitor3)
