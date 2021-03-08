d88be7bed159cfa62e9432265cc1a7c32ea8559e
blakedt2280 <blake_turner@stu.indianhills.edu>
2021-03-08 09:32:47 -0600

    finished views need pages

--- pc_builder/builder/urls.py
+++ pc_builder/builder/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import BuildListView
 from . import views
 urlpatterns = [
-    path('', views.home, name = 'builder-home'),
+    path('', BuildListView.as_view(), name = 'builder-home'),
     path('builds/', views.builds, name = 'builder-builds'),
     path('about/', views.about, name = 'builder-about'),
+    path('cases/', views.cases, name = 'builder-cases'),
+    path('cpu_coolers/', views.cpu_coolers, name = 'builder-cpu_coolers'),
+    
+
+
 ]
