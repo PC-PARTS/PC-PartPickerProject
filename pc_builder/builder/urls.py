e9ff08fd3a95d9b491b513e69a6d9ec3128547a3
blakedt2280 <blake_turner@stu.indianhills.edu>
2021-03-08 09:46:06 -0600

    added urls

--- pc_builder/builder/urls.py
+++ pc_builder/builder/urls.py
@@ -4,10 +4,19 @@ from . import views
 urlpatterns = [
     path('', BuildListView.as_view(), name = 'builder-home'),
     path('builds/', views.builds, name = 'builder-builds'),
-    path('about/', views.about, name = 'builder-about'),
-    path('cases/', views.cases, name = 'builder-cases'),
+    path('about/', views.about, name = 'builder-about'),    
     path('cpu_coolers/', views.cpu_coolers, name = 'builder-cpu_coolers'),
-    
-
-
+    path('cpu/', views.cpu, name = 'builder-cpu'),
+    path('mobo/', views.mobo, name = 'builder-mobo'),
+    path('mem1/', views.mem1, name = 'builder-mem1'),
+    path('mem2/', views.mem2, name = 'builder-mem2'),
+    path('storage1/', views.storage1, name = 'builder-storage1'),
+    path('storage2/', views.storage2, name = 'builder-storage2'),
+    path('storage3/', views.storage3, name = 'builder-storage3'),
+    path('gpu/', views.gpu, name = 'builder-gpu'),
+    path('cases/', views.cases, name = 'builder-cases'),
+    path('psu/', views.psu, name = 'builder-psu'),
+    path('mon1/', views.mon1, name = 'builder-mon1'),
+    path('mon2/', views.mon2, name = 'builder-mon2'),
+    path('mon3/', views.mon3, name = 'builder-mon3'),
 ]
